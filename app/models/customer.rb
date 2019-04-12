1fb1ac53910a6402e86dc0a3aa83a240693f95eb
PYN <yxp7531540@gmail.com>
2019-04-12 10:11:48 -0500

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,54 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def all_reviews
+    Review.all.select do |review|
+
+      review.customer == self
+
+    end
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, content, rating)
+  end
+
+  def num_reviews
+    self.all_reviews.count
+  end
+
+  def restaurants
+    self.all_reviews.map {|review| review.restaurant}.uniq
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(full_name)
+
+    @@all.find {|customer| customer.full_name == full_name}
+  end
+
+  def self.find_all_by_first_name(first_name)
+    @@all.select {|customer| customer.first_name == first_name}
+  end
+
+  def self.all_names
+    @@all.map {|customer| customer.full_name}
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,36 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    self.reviews.map {|review| review.customer}.uniq
+  end
+
+  def average_star_rating
+
+    (self.reviews.map {|review| review.rating}.sum).to_f / self.reviews.count
+  end
+
+  def longest_review
+    self.reviews.max_by {|review| review.content.length}
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|restaurant| restaurant.name == name}
+  end
 end
