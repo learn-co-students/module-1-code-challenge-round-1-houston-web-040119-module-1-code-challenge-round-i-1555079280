a3b0b8b22f0e1755dd4f07deba16f100589ef22b
kreestoff <christopher.l.mcintosh@outlook.com>
2019-04-12 10:27:23 -0500

    tests_passing

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,34 @@
 class Restaurant
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select {|r| r.restaurant == self}
+  end
+
+  def customers
+    reviews.map {|r| r.customer}.uniq
+  end
+
+  def average_star_rating
+    reviews.map {|r| r.rating}.sum / reviews.count.to_f
+  end
+
+  def self.find_by_name(name)
+    all.select {|r| r.name == name}
+  end
+
+  def longest_review
+    reviews.max_by {|r| r.content.length}
+  end
+
 
 end
