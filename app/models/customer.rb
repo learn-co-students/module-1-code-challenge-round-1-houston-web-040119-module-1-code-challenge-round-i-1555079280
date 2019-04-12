fdda33dca907c9fd5b525764fb720277bfadf77e
smrbts <samueljustinroberts@gmail.com>
2019-04-12 10:30:34 -0500

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,54 @@
+
 class Customer
   attr_reader :first_name, :last_name
+
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def reviews
+    Review.all.select {|review| review.customer == self}
+  end
+
+  def add_review(restaurant, rating, content)
+  Review.new(self, restaurant, rating, content)
+  # - `Customer#add_review(restaurant, content, rating)`
+#   - given a **restaurant object**, some review content (as a string), and a star rating (as an integer), creates a new review and associates it with that customer and restaurant.
+  end
+
+  def num_reviews
+    total = 0
+    self.reviews.each{|review| total += reviews.count}
+    total
+  # - `Customer#num_reviews`
+#   - Returns the total number of reviews that a customer has authored
+  end
+
+  def restaurants
+  result = Review.all.select{|review| review.customer == self}
+
+  customer = result.map.uniq{|result| result.restaurant}
+
+  # - `Customer#restaurants`
+#   - Returns a **unique** array of all restaurants a customer has reviewed
+  end
+
+
+
+
+
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,25 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+c1 = Customer.new("Bob", "Marley")
+c2 = Customer.new("Joe", "Young")
+c3 = Customer.new("Patrick", "Star")
+c4 = Customer.new("Bubba", "Bass")
+
+r1 = Restaurant.new("The Krusty Krab")
+r2 = Restaurant.new("The Chumbucket")
+r3 = Restaurant.new("Super Weenie Hut Jr")
+
+review1 = Review.new(c1, r1, 'BEST BURGER EVER!!', 5)
+review2 = Review.new(c2, r1, 'They forgot the pickles', 4)
+review3 = Review.new(c3, r2, 'Is this actually food?', 3)
+review4 = Review.new(c4, r3, 'Why am I even here?', 1)
+
+
+
+
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
